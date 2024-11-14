27c30b9ee83cafa8ce68c05addc2369e4aef93f6
Maria Granados <mariafgrana2@gmail.com>
2024-11-13 23:04:34 -0600

    feat: work at entity class Address

--- empowerfit/src/main/java/com/empowerfit/app/model/Address.java
+++ empowerfit/src/main/java/com/empowerfit/app/model/Address.java
@@ -1,5 +1,85 @@
 package com.empowerfit.app.model;
 
-public class Address {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Address {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String streetAddress;
+	private String city;
+	private String state;
+	private int zipCode;
+	private String country;
+	//TODO preguntar sobre atributo de street adrees es mejor dividirlo en dos lineas
+	
+	protected Address() {}
+
+	public Address(String streetAddress, String city, String state, int zipCode, String country) {
+		this.streetAddress = streetAddress;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+		this.country = country;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Address [id=%s, streetAddress=%s, city=%s, state=%s, zipCode=%s, country=%s]", id,
+				streetAddress, city, state, zipCode, country);
+	}
+	
 }
+
