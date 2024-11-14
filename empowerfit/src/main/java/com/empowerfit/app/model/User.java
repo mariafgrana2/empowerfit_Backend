9a5bc35a0ab875a26a50d4b600c20f42985825fa
Maria Granados <mariafgrana2@gmail.com>
2024-11-13 22:22:29 -0600

    feat: work at entity class User

--- empowerfit/src/main/java/com/empowerfit/app/model/User.java
+++ empowerfit/src/main/java/com/empowerfit/app/model/User.java
@@ -1,5 +1,81 @@
 package com.empowerfit.app.model;
 
-public class User {
+import jakarta.persistence.Entity;
 
+@Entity
+public class User {
+	private int id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+	private boolean active;
+	
+	protected User() {}
+
+	public User(String name, String email, String phone, String password, boolean active) {
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+		this.active = active;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("User [id=%s, name=%s, email=%s, phone=%s, password=%s, active=%s]", id, name, email,
+				phone, password, active);
+	}
+	
+	
+	
+	
 }
