ced9291d5ef2fcd45adc8080870dbda111b9b0e4
Maria Granados <mariafgrana2@gmail.com>
2024-11-13 22:04:30 -0600

    feat: se crea el proyecto con initializer

---
+++ empowerfit/src/main/java/com/empowerfit/app/EmpowerfitApplication.java
@@ -0,0 +1,13 @@
+package com.empowerfit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpowerfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpowerfitApplication.class, args);
+	}
+
+}
---
+++ empowerfit/src/test/java/com/empowerfit/app/EmpowerfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empowerfit.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpowerfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
