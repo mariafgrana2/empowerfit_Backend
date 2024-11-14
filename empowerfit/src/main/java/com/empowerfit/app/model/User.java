b67f5cb04d90f979cbf2d361f952ee7af14d5872
Maria Granados <mariafgrana2@gmail.com>
2024-11-13 23:00:05 -0600

    feat: work at entity class Address

--- empowerfit/src/main/java/com/empowerfit/app/model/User.java
+++ empowerfit/src/main/java/com/empowerfit/app/model/User.java
@@ -1,15 +1,21 @@
 package com.empowerfit.app.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	private boolean active;
+	//TODO preguntar por atributo cumpleaños
 	
 	protected User() {}
 
