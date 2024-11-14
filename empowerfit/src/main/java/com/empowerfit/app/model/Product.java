0a1a641c3678b5bc73d5e91cd76553a695b42596
Maria Granados <mariafgrana2@gmail.com>
2024-11-13 23:22:43 -0600

    feat: work at entity class Product

--- empowerfit/src/main/java/com/empowerfit/app/model/Product.java
+++ empowerfit/src/main/java/com/empowerfit/app/model/Product.java
@@ -1,5 +1,76 @@
 package com.empowerfit.app.model;
 
-public class Product {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imageUrl;
+	
+	//TODO revisar reestructura de propiedades y stock
+	
+	protected Product () {}
+
+	public Product(String name, String description, Double price, String imageUrl) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imageUrl = imageUrl;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Product [id=%s, name=%s, description=%s, price=%s, imageUrl=%s]", id, name, description,
+				price, imageUrl);
+	}
+	
+	
 }
