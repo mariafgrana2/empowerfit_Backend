69b725d362facaac01813d70dd2e0190b0f47475
Maria Granados <mariafgrana2@gmail.com>
2024-11-15 02:54:01 -0600

    feat: se agrega repository de la entidad user

--- empowerfit/src/main/java/com/empowerfit/app/repository/UserRepository.java
+++ empowerfit/src/main/java/com/empowerfit/app/repository/UserRepository.java
@@ -4,6 +4,24 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.empowerfit.app.model.User;
 
-public interface UserRepository extends CrudRepository<User, Long>{
+import java.util.Optional;
+import java.util.Set;
 
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    // Encuentra un usuario por su correo electrónico.
+    User findByEmail(String email);
+
+    // Encuentra un usuario por su número de teléfono.
+    User findByPhone(String phone);
+
+    // Encuentra todos los usuarios activos.
+    Set<User> findAllByActiveTrue();
+
+    //Encuentra todos los usuarios inactivos.
+    Set<User> findAllByActiveFalse();
+    
+    // Encuentra un usuario por su ID.
+    Optional<User> findById(Long id);
+    
 }
