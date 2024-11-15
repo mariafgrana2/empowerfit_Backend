13d04823cd27cea0ac9c274d80bf4f1a10f27bc4
Lisette Neri <lisette_neri_m@gmail.com>
2024-11-15 02:50:52 -0600

    feat:PayPage y ProductsProperty en Repository

--- empowerfit/src/main/java/com/empowerfit/app/repository/ProductPropertyRepository.java
+++ empowerfit/src/main/java/com/empowerfit/app/repository/ProductPropertyRepository.java
@@ -1,5 +1,24 @@
 package com.empowerfit.app.repository;
 
-public interface ProductPropertyRepository {
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.empowerfit.app.model.ProductProperty;
+
+public interface ProductPropertyRepository extends CrudRepository<ProductProperty, Long> {
+
+    // SELECT * FROM product_property WHERE material = ?
+    List<ProductProperty> findByMaterial(String material);
+
+    // SELECT * FROM product_property WHERE size_id = ?
+    List<ProductProperty> findBySizeId(Long sizeId);
+
+    // SELECT * FROM product_property WHERE id = ?
+    Optional<ProductProperty> findById(Long id);
+
+    // Obtiene todas las propiedades con un material específico
+    Set<ProductProperty> findAllByMaterial(String material);
 }
