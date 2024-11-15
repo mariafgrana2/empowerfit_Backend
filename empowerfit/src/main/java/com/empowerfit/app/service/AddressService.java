44fe89279f56525c5c1c435d6e6b9523bb25af4b
Karina Ibarra <karinaibarrait@gmail.com>
2024-11-15 01:58:18 -0600

    service adress & shopbag

--- empowerfit/src/main/java/com/empowerfit/app/service/AddressService.java
+++ empowerfit/src/main/java/com/empowerfit/app/service/AddressService.java
@@ -1,5 +1,14 @@
 package com.empowerfit.app.service;
 
+import java.util.Set;
+import com.empowerfit.app.model.Address;
+
 public interface AddressService {
+
+	Address createAddress(Address address);
+	Address getAddressById(Long id);
+	Set<Address> getAllAddressesByCountry(String country);
+	Address updateAddress(Address address, Long id);
+	void deleteAddress(Long id);
 
 }
--- empowerfit/src/main/java/com/empowerfit/app/service/ShopBagService.java
+++ empowerfit/src/main/java/com/empowerfit/app/service/ShopBagService.java
@@ -1,5 +1,16 @@
 package com.empowerfit.app.service;
 
+import java.util.Date;
+import java.util.List;
+import com.empowerfit.app.model.ShopBag;
+
 public interface ShopBagService {
+
+	ShopBag createShopBag(ShopBag shopBag);
+	ShopBag getShopBagById(Long id);
+	List<ShopBag> getShopBagsByDate(Date date);
+	List<ShopBag> getShopBagsByDateRange(Date startDate, Date endDate);
+	ShopBag updateShopBag(ShopBag shopBag, Long id);
+	void deleteShopBag(Long id);
 
 }
