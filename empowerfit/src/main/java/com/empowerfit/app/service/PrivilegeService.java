f92fee32eb2dcf9c620842fd0f5a2602ca0a9000
Sara Vergara <sarasilvia.vergara@gmail.com>
2024-11-15 03:01:45 -0600

    feat: privilege & status service

--- empowerfit/src/main/java/com/empowerfit/app/service/PrivilegeService.java
+++ empowerfit/src/main/java/com/empowerfit/app/service/PrivilegeService.java
@@ -1,5 +1,14 @@
 package com.empowerfit.app.service;
 
+import java.util.Set;
+import com.empowerfit.app.model.Privilege;
+
 public interface PrivilegeService {
+	
+	Privilege createPrivilege(Privilege privilege);
+    Privilege getPrivilegeById(Long id);
+    Privilege getPrivilegeByName(String privilegeName);
+    Privilege updatePrivilege(Privilege privilege, Long id);
+    void deletePrivilege(Long id);
 
 }
--- empowerfit/src/main/java/com/empowerfit/app/service/StatusService.java
+++ empowerfit/src/main/java/com/empowerfit/app/service/StatusService.java
@@ -1,5 +1,14 @@
 package com.empowerfit.app.service;
 
+import java.util.Set;
+import com.empowerfit.app.model.Status;
+
 public interface StatusService {
+	
+	 Status createStatus(Status status);
+	 Status getStatusById(Long id);
+	 Status getStatusByName(String nameStatus);
+	 Status updateStatus(Status status, Long id);
+	 void deleteStatus(Long id);
 
 }
