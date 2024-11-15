8be72e0fc322edf1f0aa2e651c2770f3d34a4e25
gabriela nunez <gnunezoliv@gmail.com>
2024-11-15 00:32:02 -0600

    Modificacion de model en: Color y Size

--- empowerfit/src/main/java/com/empowerfit/app/model/Size.java
+++ empowerfit/src/main/java/com/empowerfit/app/model/Size.java
@@ -1,5 +1,42 @@
 package com.empowerfit.app.model;
+@Entity
+@Table(name="sizes")
 
-public class Size {
+public class Size {	
+	
+	@Id
+ 	@GeneratedValue(strategy=GenerationType.IDENTITY)
+ 	
+	private Long id;
+ 	
+	@Column(name="name_size", length=45, nullable=false)
+ 	private String size;
+	
+	Size () {}
 
+	public Size(String size) {
+		this.size = size;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Size [id=%s, size=%s]", id, size);
+	}
+	
 }
